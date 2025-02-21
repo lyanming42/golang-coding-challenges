a0ccff01aca72404a58b883e0d75b21590dd1a98
YanMing Li <lyanming42@gmail.com>
2025-02-21 11:26:43 -0800

    add more examples

--- maxsubarray/maxsubarray.go
+++ maxsubarray/maxsubarray.go
@@ -5,7 +5,32 @@ import (
 	"slices"
 )
 
+type Person struct {
+	name   string
+	age    int
+	job    string
+	salary int
+}
+
+func printPerson(pers Person) {
+	fmt.Println(pers.age)
+	fmt.Println(pers.name)
+	fmt.Println(pers.job)
+	fmt.Println(pers.salary)
+}
+
+func factorial_recursion(x float64) (y float64) {
+	if x > 0 {
+		y = x * factorial_recursion(x-1)
+	} else {
+		y = 1
+	}
+	return
+}
+
 func main() {
+	const PI = 3.14
+
 	var nums []int = []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	for i := 1; i < len(nums); i++ {
 		if nums[i-1] > 0 {
@@ -14,4 +39,35 @@ func main() {
 	}
 	max := slices.Max(nums)
 	fmt.Println(max)
+
+	var pers1 Person
+
+	pers1.age = 10
+	pers1.name = "abcd"
+	pers1.job = "hkr"
+	pers1.salary = 12
+
+	fmt.Println(pers1.age)
+	fmt.Println(pers1.name)
+	fmt.Println(pers1.job)
+	fmt.Println(pers1.salary)
+
+	printPerson((pers1))
+
+	var a = map[string]string{"Ford": "Ford", "Team": "Chelsea"}
+	b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
+
+	fmt.Println(a["Ford"])
+	fmt.Println(b)
+
+	fmt.Println(factorial_recursion(4))
+
+	arr1 := [6]int{10, 11, 12, 13, 14, 15}
+	myslice := arr1[2:4]
+
+	copiedNumbers := make([]int, 2, 2)
+	copy(copiedNumbers, myslice)
+
+	fmt.Println(myslice)
+	fmt.Println(copiedNumbers)
 }
