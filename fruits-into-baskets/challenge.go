e4106200166c06e40612f6a957351a0df55b1444
YanMing Li <lyanming42@gmail.com>
2025-03-03 12:24:33 -0800

    done with the solution

--- fruits-into-baskets/challenge.go
+++ fruits-into-baskets/challenge.go
@@ -5,6 +5,25 @@ import (
 )
 
 func main() {
-	fruits := []int{0, 1, 2, 2}
-	fmt.Println(fruits)
+	fruits := []int{1, 2, 3, 2, 2}
+	fruitsCount := map[int]int{}
+	res := 0
+
+	start := 0
+	for end := 0; end < len(fruits); end++ {
+		fruitsCount[fruits[end]]++
+
+		for ; start <= end && len(fruitsCount) > 2; start++ {
+			fruitsCount[fruits[start]]--
+
+			if fruitsCount[fruits[start]] == 0 {
+				delete(fruitsCount, fruits[start])
+			}
+		}
+		if res < (end - start + 1) {
+			res = end - start + 1
+		}
+	}
+
+	fmt.Println(res)
 }
