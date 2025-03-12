86e6697bbe50c0ed5b5ede3524eca94fc60eef80
YanMing Li <lyanming42@gmail.com>
2025-03-12 02:49:57 -0700

    done with permutation in string solution

--- permutation-in-string/permutation-in-string.go
+++ permutation-in-string/permutation-in-string.go
@@ -1,11 +1,28 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	s1, s2 := "ab", "eidbaooo"
+	start, count := 0, [26]int{}
 
-	fmt.Println(s1, s2)
+	for idx := range s1 {
+		count[s1[idx]-97]++
+	}
+
+	for end := range s2 {
+		count[s2[end]-97]--
+
+		if (end - start + 1) > len(s1) {
+			count[s2[start]-97]++
+			start++
+		}
+
+		if count == [26]int{} {
+			fmt.Println("True")
+			return
+		}
+	}
+
+	fmt.Println("False")
 }
