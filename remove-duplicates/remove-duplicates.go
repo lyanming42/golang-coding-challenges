97c3f6af48cb8627ef2e343d8a47bae2ce463768
YanMing Li <lyanming42@gmail.com>
2025-03-25 19:59:10 -0700

    done with remove duplicates

--- remove-duplicates/remove-duplicates.go
+++ remove-duplicates/remove-duplicates.go
@@ -5,7 +5,16 @@ import "fmt"
 func main() {
 	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
 
-	cur := nums[0]
+	cur := nums[0] - 1
 
-	fmt.Println(nums, cur)
+	cnt := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != cur {
+			cur = nums[i]
+			nums[cnt] = cur
+			cnt++
+		}
+	}
+	fmt.Println(nums, cnt)
 }
